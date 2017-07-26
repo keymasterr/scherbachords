56bc855b0dc5b158ab992084da82c8a8eddf3afc
Roman Kliuchkovych <kimasterr@gmail.com>
2017-07-26 13:13:34 +0300

    Favicons update, minor changes to texts and css

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     chordsCreation();
-    doubleHover('a','hover');
+    doubleHover('a', 'hover');
     $(window).hashchange();
 });
 
@@ -13,22 +13,20 @@ var chords,
 function activateChords(thisObj) {
     if (thisObj) {
         currTrackId = thisObj.attr('href').substring(1);
-    }
-    else if (document.location.hash.length > 3 && ['#top', '#a…z', '#0…9'].indexOf(document.location.hash) <= -1) {
-      currTrackId = document.location.hash.substring(1);
-    }
-    else {
+    } else if (document.location.hash.length > 3 && ['#top', '#a…z', '#0…9'].indexOf(document.location.hash) <= -1) {
+        currTrackId = document.location.hash.substring(1);
+    } else {
         $.modal().close();
         return false;
     }
-    var currChords = chords.find('track[id="' +currTrackId+'"]');
+    var currChords = chords.find('track[id="' + currTrackId + '"]');
 
-    var currTrackHeader = '<div class="chords_header"><h2 href="#' +currTrackId+ '">' +currChords.find('title').text()+ '<span class="track_starred-star">&#9733;</span></h2><h3>' +currChords.find('title-alt2').text()+ '</h3><h4>' +currChords.find('year').text()+ '</h4><p class="subtitle">' +currChords.find('subtitle').text()+ '</p></div>';
+    var currTrackHeader = '<div class="chords_header"><h2 href="#' + currTrackId + '">' + currChords.find('title').text() + '<span class="track_starred-star">&#9733;</span></h2><h3>' + currChords.find('title-alt2').text() + '</h3><h4>' + currChords.find('year').text() + '</h4><p class="subtitle">' + currChords.find('subtitle').text() + '</p></div>';
     var currTrackText = '<div class="chords_text">' + currChords.find('text').text() + '</div>';
     var currTrack = currTrackHeader + currTrackText;
 
     $('.modal').empty().html(currTrack).modal().open();
-    $(document).prop('title',  chords.find('track[id="' +currTrackId+'"]').find('title').text() + ' — Щербаккорды');
+    $(document).prop('title', chords.find('track[id="' + currTrackId + '"]').find('title').text() + ' — Щербаккорды');
 
     setStarred();
 
@@ -39,41 +37,41 @@ function activateChords(thisObj) {
         }
     });
     $('.modal').focus();
-};
+}
 
 var clearAddress = function() {
-  // document.location.hash = "";
-  currTrackId = "";
-  $(document).prop('title', 'Щербаккорды');
-  history.pushState('', '', window.location.href.split('#')[0]);
+    // document.location.hash = "";
+    currTrackId = "";
+    $(document).prop('title', 'Щербаккорды');
+    history.pushState('', '', window.location.href.split('#')[0]);
 };
 
 function chordsCreation() {
-    chords = $( getXML() );
-    var track  = chords.find( 'track' ),
-        byAbc  = [],
+    chords = $(getXML());
+    var track = chords.find('track'),
+        byAbc = [],
         byYear = [],
-        sortedByAbc  = [],
+        sortedByAbc = [],
         sortedByYear = [],
-        prevCharAbc  = '',
+        prevCharAbc = '',
         prevCharYear = '';
 
     track.each(function() {
-        var id        = $(this).attr('id'),
-            title     = $(this).find('title').text(),
-            year      = $(this).find('year').text(),
+        var id = $(this).attr('id'),
+            title = $(this).find('title').text(),
+            year = $(this).find('year').text(),
             titleAlt1 = $(this).find('title-alt1').text(),
             titleAlt2 = $(this).find('title-alt2').text(),
-            liTempl   = '<li><a %%<span class="track_starred-star">&#9733;</span></a></li>';
+            liTempl = '<li><a %%<span class="track_starred-star">&#9733;</span></a></li>';
 
         byAbc.push(liTempl.splice(7, 2,
-            'href="#' +id+ '" data-year="' +year+ '">' +firstQuote(title)
+            'href="#' + id + '" data-year="' + year + '">' + firstQuote(title)
         ));
         titleAlt1 && byAbc.push(liTempl.splice(7, 2,
-            'href="#' +id+ '" data-year="' +year+ '">' +firstQuote(titleAlt1)+ ' (' +title+ ')'
+            'href="#' + id + '" data-year="' + year + '">' + firstQuote(titleAlt1) + ' (' + title + ')'
         ));
         titleAlt2 && byAbc.push(liTempl.splice(7, 2,
-            'href="#' +id+ '" data-year="' +year+ '">' +firstQuote(titleAlt2)+ ' (' +title+ ')'
+            'href="#' + id + '" data-year="' + year + '">' + firstQuote(titleAlt2) + ' (' + title + ')'
         ));
 
         var match = year.match(/\d{4}/g);
@@ -81,17 +79,17 @@ function chordsCreation() {
             var sortYear = itm + '';
             if (titleAlt1 && titleAlt2) {
                 byYear.push(liTempl.splice(7, 2,
-                    'href="#' +id+ '" data-year-sort="' +sortYear+ '">' +title+ ', ' +titleAlt2+ ' (' +titleAlt1+ ')'
+                    'href="#' + id + '" data-year-sort="' + sortYear + '">' + title + ', ' + titleAlt2 + ' (' + titleAlt1 + ')'
                 ));
             } else if (titleAlt1) {
                 byYear.push(liTempl.splice(7, 2,
-                    'href="#' +id+ '" data-year-sort="' +sortYear+ '">' +title+ ' (' +titleAlt1+ ')'
+                    'href="#' + id + '" data-year-sort="' + sortYear + '">' + title + ' (' + titleAlt1 + ')'
                 ));
             } else {
                 byYear.push(liTempl.splice(7, 2,
-                    'href="#' +id+ '" data-year-sort="' +sortYear+ '">' +title
+                    'href="#' + id + '" data-year-sort="' + sortYear + '">' + title
                 ));
-            };
+            }
         });
     });
 
@@ -107,19 +105,19 @@ function chordsCreation() {
             curChar = '0…9';
         } else if (curChar.match(/^[a-zA-Z]/)) {
             curChar = 'A…Z';
-        };
+        }
 
         if (prevCharAbc == curChar) {
-            sortedByAbc.push(itm)
+            sortedByAbc.push(itm);
         } else {
             prevCharAbc = curChar;
-            if ( prevCharAbc != '') {
+            if (prevCharAbc != '') {
                 sortedByAbc.push('</ul></dd>');
             }
-            sortedByAbc.push('<dt><a>' +curChar+ '</a></dt>');
-            sortedByAbc.push('<dd><ul>')
+            sortedByAbc.push('<dt><a>' + curChar + '</a></dt>');
+            sortedByAbc.push('<dd><ul>');
             sortedByAbc.push(itm);
-        };
+        }
     });
 
     sortedByAbcHtml = sortedByAbc.join('');
@@ -130,7 +128,7 @@ function chordsCreation() {
             by = $(b).find('a').data('yearSort') + '';
         if (ay != by) {
             return ay - by;
-        };
+        }
         a = trimSpecial($(a).text());
         b = trimSpecial($(b).text());
         return a.toUpperCase().localeCompare(b.toUpperCase());
@@ -139,16 +137,16 @@ function chordsCreation() {
     $.each(byYear, function(idx, itm) {
         var curChar = trimSpecial($('a', itm).data('yearSort'));
         if (prevCharYear == curChar) {
-            sortedByYear.push(itm)
+            sortedByYear.push(itm);
         } else {
             prevCharYear = curChar;
-            if ( prevCharYear != '') {
+            if (prevCharYear != '') {
                 sortedByYear.push('</ul></dd>');
             }
-            sortedByYear.push('<dt><a>' +$('a', itm).data('yearSort')+ '</a></dt>');
-            sortedByYear.push('<dd><ul>')
+            sortedByYear.push('<dt><a>' + $('a', itm).data('yearSort') + '</a></dt>');
+            sortedByYear.push('<dd><ul>');
             sortedByYear.push(itm);
-        };
+        }
     });
 
     sortedByYearHtml = sortedByYear.join('');
@@ -163,17 +161,17 @@ function chordsCreation() {
     $('body').append('<div class="modal" tabindex="1"></div>');
     if (Cookies.get('sorting') == 'year') {
         sortToggle();
-    };
+    }
     sortToggle();
 
     setStarred();
     goToLetter();
 
 
-    $(window).hashchange( function() {
-      activateChords();
-    })
-};
+    $(window).hashchange(function() {
+        activateChords();
+    });
+}
 
 function setStarred() {
     var starClass = 'track_starred',
@@ -181,15 +179,15 @@ function setStarred() {
 
     if (typeof starray === 'undefined') {
         starray = [];
-    };
+    }
     if (typeof Cookies(starCookie) !== 'undefined') {
         starray = JSON.parse(Cookies.get(starCookie));
-    };
+    }
 
     $('.' + starClass).removeClass(starClass);
     for (var i = 0; i < starray.length; i++) {
-        $('a[href="#' +starray[i]+ '"], h2[href="#' +starray[i]+ '"]').addClass(starClass);
-    };
+        $('a[href="#' + starray[i] + '"], h2[href="#' + starray[i] + '"]').addClass(starClass);
+    }
 
     $('h2 .track_starred-star').click(function() {
         var par = $(this).parent(),
@@ -201,118 +199,118 @@ function setStarred() {
         } else {
             par.addClass(starClass);
             starray.push(id);
-        };
+        }
         Cookies.set(starCookie, JSON.stringify(starray));
     });
-};
+}
 
 function abcIndex() {
-  var aabb = {
-    'А' : 'a',
-    'Б' : 'b',
-    'В' : 'v',
-    'Г' : 'g',
-    'Д' : 'd',
-    'Е' : 'e',
-    'Ж' : 'zh',
-    'З' : 'z',
-    'И' : 'i',
-    'К' : 'k',
-    'Л' : 'l',
-    'М' : 'm',
-    'Н' : 'n',
-    'О' : 'o',
-    'П' : 'p',
-    'Р' : 'r',
-    'С' : 's',
-    'Т' : 't',
-    'У' : 'u',
-    'Ф' : 'f',
-    'Х' : 'x',
-    'Ц' : 'c',
-    'Ч' : 'ch',
-    'Ш' : 'sh',
-    'Щ' : 'w',
-    'Э' : 'eh',
-    'Ю' : 'ju',
-    'Я' : 'ja'
-  };
+    var aabb = {
+        'А': 'a',
+        'Б': 'b',
+        'В': 'v',
+        'Г': 'g',
+        'Д': 'd',
+        'Е': 'e',
+        'Ж': 'zh',
+        'З': 'z',
+        'И': 'i',
+        'К': 'k',
+        'Л': 'l',
+        'М': 'm',
+        'Н': 'n',
+        'О': 'o',
+        'П': 'p',
+        'Р': 'r',
+        'С': 's',
+        'Т': 't',
+        'У': 'u',
+        'Ф': 'f',
+        'Х': 'x',
+        'Ц': 'c',
+        'Ч': 'ch',
+        'Ш': 'sh',
+        'Щ': 'w',
+        'Э': 'eh',
+        'Ю': 'ju',
+        'Я': 'ja'
+    };
 
-  $('.abc_index ul').empty();
+    $('.abc_index ul').empty();
 
-  if (Cookies.get('sorting') == 'abc') $('dl.content_contents dt a').each(function(){
+    if (Cookies.get('sorting') == 'abc') $('dl.content_contents dt a').each(function() {
 
-    var l = $(this).text().toUpperCase();
-    $(this).attr('name', function() {
-      if (l in aabb) return aabb[l]
-      else if (/^[a-zA-Z]/.test(l)) return 'a…z'
-      else if (/^[0-9]/.test(l)) return '0…9'
-      else return '';
-    }).attr('href', '#top');
+        var l = $(this).text().toUpperCase();
+        $(this).attr('name', function() {
+            if (l in aabb) return aabb[l];
+            else if (/^[a-zA-Z]/.test(l)) return 'a…z';
+            else if (/^[0-9]/.test(l)) return '0…9';
+            else return '';
+        }).attr('href', '#top');
 
-    $('.abc_index ul').append('<li><a href="#' + $(this).attr('name') + '">' + l + '</a></li>');
-  });
-};
+        $('.abc_index ul').append('<li><a href="#' + $(this).attr('name') + '">' + l + '</a></li>');
+    });
+}
 
 function goToLetter() {
-  var keys = {},
-      a = {
-    48  : 'top',
-    49  : 'top',
-    50  : 'top',
-    51  : 'top',
-    52  : 'top',
-    53  : 'top',
-    54  : 'top',
-    55  : 'top',
-    56  : 'top',
-    57  : 'top',
-    192 : 'top',
-    70  : 'a',
-    188 : 'b',
-    68  : 'v',
-    85  : 'g',
-    76  : 'd',
-    84  : 'e',
-    186 : 'zh',
-    80  : 'z',
-    66  : 'i',
-    82  : 'k',
-    75  : 'l',
-    86  : 'm',
-    89  : 'n',
-    74  : 'o',
-    71  : 'p',
-    72  : 'r',
-    67  : 's',
-    78  : 't',
-    69  : 'u',
-    65  : 'f',
-    219 : 'x',
-    87  : 'c',
-    88  : 'ch',
-    73  : 'sh',
-    79  : 'w',
-    222 : 'eh',
-    190 : 'ju',
-    90  : 'ja'
-  };
+    var keys = {},
+        a = {
+            48: 'top',
+            49: 'top',
+            50: 'top',
+            51: 'top',
+            52: 'top',
+            53: 'top',
+            54: 'top',
+            55: 'top',
+            56: 'top',
+            57: 'top',
+            192: 'top',
+            70: 'a',
+            188: 'b',
+            68: 'v',
+            85: 'g',
+            76: 'd',
+            84: 'e',
+            186: 'zh',
+            80: 'z',
+            66: 'i',
+            82: 'k',
+            75: 'l',
+            86: 'm',
+            89: 'n',
+            74: 'o',
+            71: 'p',
+            72: 'r',
+            67: 's',
+            78: 't',
+            69: 'u',
+            65: 'f',
+            219: 'x',
+            87: 'c',
+            88: 'ch',
+            73: 'sh',
+            79: 'w',
+            222: 'eh',
+            190: 'ju',
+            90: 'ja'
+        };
 
 
-  $(document).keydown(function(e) {
-    var k = a[e.which];
-    if (JSON.stringify(keys).length <= 2 && Cookies.get('sorting') == 'abc') {
-      keys[e.which] = true;
+    $(document).keydown(function(e) {
+        var k = a[e.which];
+        if (JSON.stringify(keys).length <= 2 && Cookies.get('sorting') == 'abc') {
+            keys[e.which] = true;
 
-      if (k) {
-        location.hash = "#" + k;
-      }
-    };
-  });
+            if (k) {
+                location.hash = "#" + k;
+            }
+        }
+    });
 
-  $(document).keyup(function (e) {
-      delete keys[e.which];
-  });
+    $(document).keyup(function(e) {
+        delete keys[e.which];
+    });
 
 }
 
@@ -320,37 +318,37 @@ function goToLetter() {
 function firstQuote(text) {
     if (text[0] == '«') {
         text = '<span style="margin-left:-.6em;">«</span>' + text.substring(1);
-    };
+    }
     return text;
 }
 
 function sortToggle() {
-    if ( $('.sorting_toggler .active').text() == 'по годам' ) {
+    if ($('.sorting_toggler .active').text() == 'по годам') {
         $('.sorting_toggler span').toggleClass('active');
         $('.content_contents').empty().append(sortedByYearHtml);
-        Cookies.set('sorting','year');
+        Cookies.set('sorting', 'year');
     } else {
         $('.sorting_toggler span').toggleClass('active');
         $('.content_contents').empty().append(sortedByAbcHtml);
-        Cookies.set('sorting','abc');
+        Cookies.set('sorting', 'abc');
     }
     starray = undefined;
     setStarred();
     abcIndex();
-};
+}
 
 function getXML() {
     var XML = null;
     $.ajax({
-        url:     'chords.xml',
-        dataType:'xml',
-        async:    false,
-        success:  function(data) {
+        url: 'chords.xml',
+        dataType: 'xml',
+        async: false,
+        success: function(data) {
             XML = data;
         }
     });
     return XML;
-};
+}
 
 // Delete unwanted first and last symbols from string
 function trimSpecial(t) {
@@ -358,17 +356,17 @@ function trimSpecial(t) {
         t = t + '';
         if (t.match(/[\u{2605}]$/igmu)) {
             t = t.slice(0, -1);
-        };
+        }
         if (t.match(/^[0-9a-zA-Zа-яёА-ЯЁ]/)) {
             return t;
         } else {
             t = t.substring(1);
             return trimSpecial(t);
-        };
+        }
     } else {
         return '';
-    };
-};
+    }
+}
 
 // «doubleHover» by artpolikarpov
 var doubleHover = function(selector, hoverClass) {
